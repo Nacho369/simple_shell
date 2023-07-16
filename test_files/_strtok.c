c8ff99a67718c509244138989d023d7afb4100f1
Fortune <nachodev369@gmail.com>
2023-07-16 14:24:25 -0700

    Exercise (strtok)

--- test_files/_strtok.c
+++ test_files/_strtok.c
@@ -9,10 +9,11 @@ void _strncpy(char *dest, char *src, int ch_fr, int ch_to);
  *
  * Return: Array of split strings
  */
-char **_strtok(char *str, const char *delim)
+char **_strtok(char *str, const char *delim, int *num_tokens)
 {
 	char **str_arr;
-	int indx, indx2, word_num = 1;
+	int indx, indx2, rev, word_num_est = 1;
+	int tok_count, tok_start, tok_len;
 
 	if (str == NULL || delim == NULL)
 		return (NULL);
@@ -22,39 +23,56 @@ char **_strtok(char *str, const char *delim)
 		for (indx2 = 0; delim[indx2] != '\0'; indx2++)
 		{
 			if (str[indx] == delim[indx2])
-				word_num++
+			{
+				word_num_est++;
+				break;
+			}
 		}
 	}
 
-	str_arr = malloc(sizeof(char *) * word_num);
+	str_arr = malloc(sizeof(char *) * word_num_est);
 	if (str_arr == NULL)
 		return (NULL);
 
-	tok_count = 0;
-	tok_start = 0;
-	tok_len = 0;
+	tok_count = 0; /* Each iteration of the array */
+	tok_start = 0; /* Start of the tokenized string */
+	tok_len = 0; /* Length of tokenized string */
 
-	for (indx = 0; str[indx] '\0'; indx++)
+	for (indx = 0; str[indx] != '\0'; indx++)
 	{
 		for (indx2 = 0; delim[indx2] != '\0'; indx2++)
 		{
 			if (str[indx] == delim[indx2])
 			{
-				tok_len = indx - tok_start;
+				tok_len = (indx + 1) - tok_start;
 				str_arr[tok_count] = malloc(sizeof(char) * tok_len);
 				if (str_arr[tok_count] == NULL)
 				{
 					for (rev = tok_count; rev != 0; rev--)
 						free(str_arr[tok_count]);
 					free(str_arr);
-					return (-1);
+					return (NULL);
 				}
-				_strncpy(str[tok_count], str, tok_start, indx);
+				_strncpy(str_arr[tok_count], str, tok_start, indx);
 				tok_count++;
 				tok_start = indx + 1;
+				break;
 			}
 		}
 	}
+
+	tok_len = (indx + 1) - tok_start;
+	str_arr[tok_count] = malloc(sizeof(char) * tok_len);
+	if (str_arr[tok_count] == NULL)
+	{
+		for (rev = tok_count; rev != 0; rev--)
+			free(str_arr[tok_count]);
+		free(str_arr);
+		return (NULL);
+	}
+	_strncpy(str_arr[tok_count], str, tok_start, indx);
+	tok_count++;
+	*num_tokens = tok_count;
 
 	return (str_arr);
 }
@@ -70,12 +88,12 @@ char **_strtok(char *str, const char *delim)
  * Return: Pointer to the resulting string dest
  *
  */
-void _strncpy(char *dest, char *src, int ch_fr, int ch_to)
+void _strncpy(char *dest, char *src, int start, int end)
 {
-	int indx, iter;
+	int indx, indx2;
 
-	for (indx = ch_fr, iter = 0; src[indx] != '\0'; indx++, iter++)
-		dest[iter] = src[indx];
+	for (indx = start, indx2 = 0; indx < end; indx++, indx2++)
+		dest[indx2] = src[indx];
 
-	dest[iter] = '\0';
+	dest[indx2] = '\0';
 }
